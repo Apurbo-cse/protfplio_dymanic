c6b12e1536c27a0efe9d59fd40004b9b7c4d61c7
Apurbo Kumar Anup <apurboka@gmail.com>
2023-06-13 21:35:38 +0600

    Update education.php

--- app/Models/education.php
+++ app/Models/education.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class education extends Model
 {
     use HasFactory;
+    const ACTIVE_STATUS = '1';
+    const INACTIVE_STATUS = '0';
+    protected $fillable = [
+        'title',
+        'company',
+        'location',
+        'description',
+        'status',
+    ];
 }
