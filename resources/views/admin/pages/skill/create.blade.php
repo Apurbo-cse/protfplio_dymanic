3b34f8ef600f75fcbb2c068580e635a52d21e042
Apurbo Kumar Anup <apurboka@gmail.com>
2023-06-13 19:58:40 +0600

    Update create.blade.php

--- resources/views/admin/pages/skill/create.blade.php
+++ resources/views/admin/pages/skill/create.blade.php
@@ -11,7 +11,7 @@
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item"><a href="#">skill</a></li>
                         <li class="breadcrumb-item active">Validation</li>
                     </ol>
                 </div>
@@ -27,13 +27,13 @@
                         <div class="card-header">
                             <h3 class="card-title">Quick Example <small>jQuery Validation</small></h3>
                         </div>
-                        <form id="quickForm" action="{{ route('admin.home.store') }}" method="post" enctype="multipart/form-data">
+                        <form id="quickForm" action="{{ route('admin.skill.store') }}" method="post" enctype="multipart/form-data">
                             @csrf
                             <div class="card-body">
                                 <div class="form-group">
                                     <label for="exampleInputEmail1">Name</label>
-                                    <input type="text" name="name" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                                    @error('name')
+                                    <input type="text" name="title" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                                    @error('title')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
                                 </div>
@@ -41,21 +41,6 @@
                                     <label for="exampleInputPassword1">Description</label>
                                     <input type="text" name="description" class="form-control" placeholder="Enter description">
                                     @error('description')
-                                        <span class="text-danger">{{ $message }}</span>
-                                    @enderror
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleInputFile">File input</label>
-                                    <div class="input-group">
-                                        <div class="custom-file">
-                                            <input type="file" class="custom-file-input" name="image" id="exampleInputFile">
-                                            <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                                        </div>
-                                        <div class="input-group-append">
-                                            <span class="input-group-text">Upload</span>
-                                        </div>
-                                    </div>
-                                    @error('image')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
                                 </div>
