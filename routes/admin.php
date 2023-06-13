5c2c5a48e819fbc8b9eff926b4b3c73b96086fea
Apurbo Kumar Anup <apurboka@gmail.com>
2023-06-13 19:58:45 +0600

    Update admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\admin\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\admin\HomeController;
+use App\Http\Controllers\admin\SkillController;
 
 
 
@@ -24,5 +25,6 @@ Route::get('/dashboard', function () {
 
 Route::group(['prefix' => 'dashboard/', 'as' => 'admin.', 'middleware' => ['auth']], function () {
     Route::resource('home', HomeController::class);
+    Route::resource('skill', SkillController::class);
 
 });
