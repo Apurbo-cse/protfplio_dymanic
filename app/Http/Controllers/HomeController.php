b2572975767809a36948868f15cb2a54e4c74149
Apurbo Kumar Anup <apurboka@gmail.com>
2023-06-13 20:21:47 +0600

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Home;
 use Illuminate\Support\Facades\DB;
+use App\Models\Home;
+use App\Models\Skill;
 
 class HomeController extends Controller
 {
     public function index()
     {
         $home = DB::table('homes')->where('status', '1')->get();
-        return view('frontend.master',compact('home'));
+        $skill = DB::table('skills')->where('status', '1')->get();
+        return view('frontend.master',compact('home', 'skill'));
     }
 }
