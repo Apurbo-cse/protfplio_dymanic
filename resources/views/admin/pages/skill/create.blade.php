8cf67f4ac1e68ca474044a61f14a807b3fcaea3a
Apurbo Kumar Anup <apurboka@gmail.com>
2023-06-13 19:51:01 +0600

    Create create.blade.php

---
+++ resources/views/admin/pages/skill/create.blade.php
@@ -0,0 +1,85 @@
+@extends('admin.master')
+
+@section('content')
+<div class="content-wrapper">
+
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Validation</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Validation</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">Quick Example <small>jQuery Validation</small></h3>
+                        </div>
+                        <form id="quickForm" action="{{ route('admin.home.store') }}" method="post" enctype="multipart/form-data">
+                            @csrf
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Name</label>
+                                    <input type="text" name="name" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                                    @error('name')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputPassword1">Description</label>
+                                    <input type="text" name="description" class="form-control" placeholder="Enter description">
+                                    @error('description')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputFile">File input</label>
+                                    <div class="input-group">
+                                        <div class="custom-file">
+                                            <input type="file" class="custom-file-input" name="image" id="exampleInputFile">
+                                            <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                        </div>
+                                        <div class="input-group-append">
+                                            <span class="input-group-text">Upload</span>
+                                        </div>
+                                    </div>
+                                    @error('image')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                                <div class="form-group d-flex">
+                                    <div class="custom-control custom-radio">
+                                        <input class="custom-control-input " type="radio" id="r1" name="status" value="1" checked>
+                                        <label for="r1" class="custom-control-label">Active</label>
+                                    </div>
+                                    <div class="custom-control custom-radio mx-2">
+                                        <input class="custom-control-input custom-control-input-danger custom-control-input-outline" type="radio" id="r0" value="0" name="status">
+                                        <label for="r0" class="custom-control-label">Inactive</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+@endsection
