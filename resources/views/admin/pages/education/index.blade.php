2b6b8b3d4e2ec2fb8e1080417e52ff07ce864107
Apurbo Kumar Anup <apurboka@gmail.com>
2023-06-13 21:43:52 +0600

    Create index.blade.php

---
+++ resources/views/admin/pages/education/index.blade.php
@@ -0,0 +1,94 @@
+@extends('admin.master')
+
+@section('content')
+<div class="content-wrapper">
+
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>DataTables</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Skill</a></li>
+                        <li class="breadcrumb-item active">DataTables</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+
+                    <div class="card">
+                        <div class="card-header d-flex justify-content-end">
+                            <h3 class="card-title"><a href="{{ route('admin.skill.create')}}" class="btn btn-info ">New Create</a></h3>
+                        </div>
+
+                        <div class="card-body">
+                            <table id="example1" class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>Sl No</th>
+                                        <th>Title</th>
+                                        <th>Description</th>
+                                        <th>Status</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @php
+                                        $serialNumber = 1;
+                                    @endphp
+                                    @foreach($skill as $data)
+                                        <tr>
+                                            <td>{{ $serialNumber }}</td>
+                                            <td>{{ $data->title }}</td>
+                                            <td>{{ $data->description }}</td>
+                                            <td>
+                                                @if($data->status == 0)
+                                                    <span class="badge bg-danger">Inactive</span>
+                                                @else
+                                                    <span class="badge bg-success">Active</span>
+                                                @endif
+                                            </td>
+                                            <td class="d-flex justify-content-around">
+                                                <a href="{{ route('admin.skill.edit',$data->id) }}"><i class="fa-regular fa-pen-to-square text-info "></i></a>
+                                                <form method="post" action="{{ route('admin.skill.destroy',$data->id) }}">
+                                                    @csrf
+                                                    @method('delete')
+                                                    <i class="fa-solid fa-trash-can text-danger" onclick="return confirm('Are you confirm to delete?')"></i>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                        @php
+                                            $serialNumber++;
+                                        @endphp
+                                    @endforeach
+                                </tbody>
+                                <tfoot>
+                                    <tr>
+                                        <th>Sl No</th>
+                                        <th>Name</th>
+                                        <th>Description</th>
+                                        <th>Status</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+</div>
+
+@endsection
