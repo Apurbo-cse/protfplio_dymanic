59252163a88961e36823f4cfc5355d28d3b9f15f
Apurbo Kumar Anup <apurboka@gmail.com>
2023-06-13 21:59:54 +0600

    Update create.blade.php

--- resources/views/admin/pages/education/create.blade.php
+++ resources/views/admin/pages/education/create.blade.php
@@ -27,30 +27,59 @@
                         <div class="card-header">
                             <h3 class="card-title">Quick Example <small>jQuery Validation</small></h3>
                         </div>
-                        <form id="quickForm" action="{{ route('admin.skill.store') }}" method="post" enctype="multipart/form-data">
+                        <form id="quickForm" action="{{ route('admin.skill.store') }}" method="post"
+                            enctype="multipart/form-data">
                             @csrf
                             <div class="card-body">
                                 <div class="form-group">
-                                    <label for="exampleInputEmail1">Name</label>
-                                    <input type="text" name="title" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                                    <label for="exampleInputEmail1">Title</label>
+                                    <input type="text" name="title" class="form-control" id="exampleInputEmail1"
+                                        placeholder="Enter email">
                                     @error('title')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
                                 </div>
                                 <div class="form-group">
+                                    <label for="exampleInputEmail1">Company (Duration)</label>
+                                    <input type="text" name="company" class="form-control" id="exampleInputEmail1"
+                                        placeholder="Enter email">
+                                    @error('company')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Address</label>
+                                    <input type="text" name="location" class="form-control" id="exampleInputEmail1"
+                                        placeholder="Enter email">
+                                    @error('location')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+
+
+
+                                <div class="form-group">
                                     <label for="exampleInputPassword1">Description</label>
-                                    <input type="text" name="description" class="form-control" placeholder="Enter description">
+                                    <textarea id="summernote">
+                Place <em>some</em> <u>text</u> <strong>here</strong>
+              </textarea>
+                                    <input type="text" name="description" class="form-control"
+                                        placeholder="Enter description">
                                     @error('description')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
                                 </div>
                                 <div class="form-group d-flex">
                                     <div class="custom-control custom-radio">
-                                        <input class="custom-control-input " type="radio" id="r1" name="status" value="1" checked>
+                                        <input class="custom-control-input " type="radio" id="r1" name="status"
+                                            value="1" checked>
                                         <label for="r1" class="custom-control-label">Active</label>
                                     </div>
                                     <div class="custom-control custom-radio mx-2">
-                                        <input class="custom-control-input custom-control-input-danger custom-control-input-outline" type="radio" id="r0" value="0" name="status">
+                                        <input
+                                            class="custom-control-input custom-control-input-danger custom-control-input-outline"
+                                            type="radio" id="r0" value="0" name="status">
                                         <label for="r0" class="custom-control-label">Inactive</label>
                                     </div>
                                 </div>
